f9e1dde6caaefcf518573c6669b3b909c7a089ae
Oscarfdo <oscarfdo25@gmail.com>
2024-10-16 15:00:50 -0700

    Trid sbx

--- Gato Tridimensional/TridTicTacToe.java
+++ Gato Tridimensional/TridTicTacToe.java
@@ -1,9 +1,3 @@
-/* Oscar Fernando Hernandez Lopez
-14 de octubre del 2024
-TridTicTacToe.java
-Juego del gato tridimensional
-para dos jugadores */
-
 import java.util.ArrayList;
 
 public class TridTicTacToe{
@@ -15,13 +9,41 @@ public class TridTicTacToe{
     private Jugador jugador1, jugador2;
     private boolean hayGanador = false;
 
-     ArrayList<Jugador> jugador = new ArrayList<>(); // Lista dinámica de jugadores
+    ArrayList<Jugador> jugador = new ArrayList<>(); // Lista dinámica de jugadores
 
+    // Convertir los arreglos a String[][] en lugar de byte[][]
+    String[][] tab1 = {
+        {"1","2","3","4"},
+        {"5","6","7","8"},
+        {"9","10","11","12"},
+        {"13","14","15","16"},
+    };
+
+    String[][] tab2 = {
+        {"17","18","19","20"},
+        {"21","22","23","24"},
+        {"25","26","27","28"},
+        {"29","30","31","32"},
+    };
+
+    String[][] tab3 = {
+        {"33","34","35","36"},
+        {"37","38","39","40"},
+        {"41","42","43","44"},
+        {"45","46","47","48"},
+    };
+
+    String[][] tab4 = {
+        {"49","50","51","52"},
+        {"53","54","55","56"},
+        {"57","58","59","60"},
+        {"61","62","63","64"},
+    };
 
     public TridTicTacToe() {
         // Asignar '-' a cada posición del arreglo
         for (int i = 0; i < tablero.length; i++) {
-        tablero[i] = '-';
+            tablero[i] = '-';
         }
     }
 
@@ -44,48 +66,74 @@ public class TridTicTacToe{
         }
     }
 
-    public void imprimirTablero() {
-        for (int i = 0; i < 3; i++) {
-            // Se imprimen cuatro líneas por cada fila del tablero
-            for (int j = 0; j < 4; j++) {
-             // Primera columna
-                if (tablero[i][0] == 'X' || tablero[i][0] == 'O') {
-                    System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s",
-                    tablero[i][0], " ", " ", tablero[i][0], "*"));
-                } else {
-                System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s",
-                    " ", " ", " ", " ", "*"));
-                }
-
-                // Segunda columna
-                if (tablero[i][1] == 'X' || tablero[i][1] == 'O') {
-                    System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s",
-                    tablero[i][1], " ", " ", tablero[i][1], "*"));
-                } else {
-                System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s",
-                    " ", " ", " ", " ", "*"));
-                }
-
-                // Tercera columna
-                if (tablero[i][2] == 'X' || tablero[i][2] == 'O') {
-                    System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s",
-                    tablero[i][2], " ", " ", tablero[i][2], " "));
-                } else {
-                    System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s",
-                    " ", " ", " ", " ", " "));
-                }
-            
-                // Nueva línea para imprimir las siguientes filas vacías o jugadas
-                System.out.println();
-            }
-        
-            // Imprimir línea separadora de asteriscos entre filas
-            if (i < 2) {
-                for (int k = 0; k < 3; k++) {
-                    System.out.print(String.format("%-1s %-1s %-1s %-1s %-1s", "*", "*", "*", "*", "*"));
-                }
-                System.out.println();
+    public void formatoTabs(int i, String[][] tab){
+        for (int j = 0; j < 4; j++) {
+            if (j < 3) {
+                System.out.print(String.format("%-3s", tab[i][j]));
+            } else {
+                System.out.print(String.format("%-9s", tab[i][j]));
             }
         }
+    }
+
+    public void imprimirTableros() {
+        for (int i = 0; i < 4; i++) {
+            formatoTabs(i, tab1);
+            formatoTabs(i, tab2);
+            formatoTabs(i, tab3);
+            formatoTabs(i, tab4);
+            System.out.println();
+        }
+        System.out.println();
+        System.out.print(String.format("%-18s", "Fondo 1"));
+        System.out.print(String.format("%-18s", "Fondo 2"));
+        System.out.print(String.format("%-18s", "Fondo 3"));
+        System.out.println(String.format("%-18s", "Fondo 4"));
+    }
+
+    public void cuadricula() {
+    // Imprimir encabezado con números de columna
+    System.out.print("   "); // Espacio para la esquina superior izquierda
+    for(int k = 0; k < 4; k++){
+        for (int i = 0; i < 4; i++) {
+            System.out.print(String.format("%-5s", "  "+i));   
+        }
+        System.out.print("        ");
+    }
+        
+    System.out.println();
+
+    // Imprimir cuadrícula con el contenido
+    for (int i = 0; i < 4; i++) {
+        // Imprimir borde superior de cada fila
+        System.out.print("  +");
+        for (int j = 0; j < 4; j++) {
+            System.out.print("----+");
+        }
+        System.out.println();
+
+        // Imprimir el índice de la fila y los valores
+        System.out.print(i + 1 + " "); // Índice de la fila
+        for (int j = 0; j < 4; j++) {
+            System.out.print(String.format("| %2s ", tab1[i][j])); // Asegura que cada celda tenga un ancho fijo
+        }
+        System.out.println("|");
+    }
+
+    // Imprimir borde inferior de la última fila
+    System.out.print("  +");
+    for (int j = 0; j < 4; j++) {
+        System.out.print("----+");
+    }
+    System.out.println();
+}
+
+
+    public static void main(String[] args) {
+        TridTicTacToe juego = new TridTicTacToe();
+
+        juego.imprimirTableros();
+        System.out.println();
+        juego.cuadricula();
     }
 }
